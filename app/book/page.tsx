b5bd1b149a48a461a212387e2e4550826086c3a3
kashifff7 <siddiquikashif951@gmail.com>
2026-06-09 22:13:48 +0530

    fix: force-dynamic on apply and book pages

--- app/book/page.tsx
+++ app/book/page.tsx
@@ -1,5 +1,7 @@
 'use client'
 
+export const dynamic = 'force-dynamic'
+
 import { useState } from 'react'
 import type { FormEvent } from 'react'
 import { Navigation } from '@/components/clyvo/navigation'
